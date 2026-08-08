cb0c6d59673bee3ad38a1194c06a9337ab69b5fb
unknown <256170481+za268301-sudo@users.noreply.github.com>
2026-08-08 07:48:29 +0500

    Add input validation for out-of-range guesses

--- game.py
+++ game.py
@@ -2,9 +2,12 @@ import random
 secret_number = random.randint(1, 100)
 
 guess = int(input("Guess a number between 1 and 100: "))
-if guess == secret_number:
-    print("Correct! You win!")
-elif guess < secret_number:
-    print("Too low!")
+if guess < 1 or guess > 100:
+    print("Invalid input! Please guess between 1 and 100.")
 else:
+  if guess == secret_number:
+    print("Correct! You win!")
+  elif guess < secret_number:
+    print("Too low!")
+  else:
     print("Too high!")
