c3c09e2282eef9d4f7becb8b95e3f87089719370
unknown <256170481+za268301-sudo@users.noreply.github.com>
2026-08-08 08:02:27 +0500

    Add replay loop with play again option

--- game.py
+++ game.py
@@ -1,13 +1,20 @@
 import random
-secret_number = random.randint(1, 100)
 
-guess = int(input("Guess a number between 1 and 100: "))
-if guess < 1 or guess > 100:
-    print("Invalid input! Please guess between 1 and 100.")
-else:
-  if guess == secret_number:
-    print("Correct! You win!")
-  elif guess < secret_number:
-    print("Too low!")
-  else:
-    print("Too high!")
+while True:
+    secret_number = random.randint(1, 100)
+    guess = int(input("Guess a number between 1 and 100: "))
+
+    if guess < 1 or guess > 100:
+        print("Invalid input! Please guess between 1 and 100.")
+    else:
+        if guess == secret_number:
+            print("Correct! You win!")
+        elif guess < secret_number:
+            print("Too low!")
+        else:
+            print("Too high!")
+
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() != "yes":
+        print("Thanks for playing!")
+        break
